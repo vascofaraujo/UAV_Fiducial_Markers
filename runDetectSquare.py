69d1bacbbd34a216fd0a989aa5ce094ea5ac689a
vascofaraujo <vasco.faden.araujo@gmail.com>
2021-02-19 19:56:59 +0000

    update

--- runDetectSquare.py
+++ runDetectSquare.py
@@ -2,6 +2,9 @@ import cv2 as cv
 import sys
 import numpy as np
 from operator import itemgetter, attrgetter
+import cv2.aruco as aruco
+
+
 
 
 #function that calculates the sum of angles of a 4 line object
@@ -70,11 +73,89 @@ def match_templates(patches):
                 findMatch = 0
                 print("NOT")
             if findMatch == 1:#it should be number of objects-1
-                return 1, i
+                cv.imshow("a", patch)
+                cv.imshow("b", patchNext)
+                return 1, i, 
         except:
             print("An exception ocurred")
     return 0, 0	
 
+def match_warped(squares):
+    markers = []
+    k = 0
+    for i in range(len(squares)):
+        contours = squares[i][6]
+        for cnt in contours:
+            cnt_len = cv.arcLength(cnt, True)
+            cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True)
+            if len(cnt) == 4 and cv.isContourConvex(cnt):
+                    cnt = cnt.reshape(-1, 2)       
+
+                    x,y,w,h = cv.boundingRect(cnt)   
+
+                    if x<0.2*squares[i][5].shape[0] or y<0.2*squares[i][5].shape[1]:
+                        continue
+                    draw = np.zeros((squares[i][5].shape[0], squares[i][5].shape[1], 3), dtype=np.uint8)
+                    cv.rectangle(draw, (x,y), (x+w,y+h), (0,255,0),2)
+
+                    cv.imshow("desenh", draw)
+                    cv.imshow("waq", squares[i][5])
+                    markers.append(squares[i])
+        k = k+1
+    return markers
+def order_points(pts):
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	# return the ordered coordinates
+	return rect
+
+
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	# compute the perspective transform matrix and then apply it
+	M = cv.getPerspectiveTransform(rect, dst)
+	warped = cv.warpPerspective(image, M, (maxWidth, maxHeight))
+	# return the warped image
+	return warped
 
 #####################################################################
 
@@ -84,8 +165,16 @@ cap.set(3, 640)
 cap.set(4, 480)
 cap.set(10, 100)
 
-
-
+#parameters initialization
+sucess, img = cap.read()
+minPerimeterRate = 0.03
+maxPerimeterRate = 4
+minCornerDistanceRate = 0.05
+minPerimeterPixels = minPerimeterRate * np.maximum(img.shape[0], img.shape[1])
+maxPerimeterPixels = maxPerimeterRate * np.maximum(img.shape[0], img.shape[1])
+parameters = aruco.DetectorParameters_create() ##S
+minDistanceToBorder = parameters.minDistanceToBorder
+minDistSq = np.maximum(img.shape[0], img.shape[1]) * np.maximum(img.shape[0], img.shape[1])
 
 
 while True:
@@ -107,9 +196,10 @@ while True:
     canny = cv.Canny(gray, 255/3, 255)
 
     #dillation
-    kernel = np.ones((3,3),np.uint8)
-    canny = cv.dilate(canny,kernel,iterations = 1)
+    #kernel = np.ones((3,3),np.uint8)
+    #canny = cv.dilate(canny,kernel,iterations = 1)
 
+    
     #countours
     contours, hierarchy = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     
@@ -121,22 +211,61 @@ while True:
     # Show in a window
     cv.imshow('Contours', drawing)
 
+    
+    
     squares = []
     triangles = []
     angle = 0
     for cnt in contours:
         cnt_len = cv.arcLength(cnt, True)
         cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True)
-        if len(cnt) == 4 and cv.contourArea(cnt) > 200:
-            cnt = cnt.reshape(-1, 2)
-            #angle = np.rad2deg(np.arctan2(cnt[1][1]-cnt[0][1], cnt[1][0]-cnt[0][0]))
-            #if (angle>88 and angle<92) or (angle>-2 and angle<2) or (angle>178 and angle<182) or (angle>268 and angle<272):          
+        if len(cnt) == 4  and cv.contourArea(cnt)>200 and cv.isContourConvex(cnt):
+            cnt = cnt.reshape(-1, 2)          
+            approxCurve = cnt
             sum_angles = quad_sum(cnt)
-            if sum_angles > 355 and sum_angles < 365:
+            minCornerDistancePixels = len(cnt) * minCornerDistanceRate
+           
+            for j in range(0,4):
+                print(j, (j+1)%4)
+                d = (approxCurve[j][0] - approxCurve[(j + 1)%4][0]) * (approxCurve[j][0] - approxCurve[(j + 1)%4][0]) + (approxCurve[j][1] - approxCurve[(j + 1)%4][1]) * (approxCurve[j][1] - approxCurve[(j + 1)%4][1])
+                minDistSq = min(minDistSq, d)
+
+            if sum_angles == 360 and minDistSq >= minCornerDistancePixels * minCornerDistancePixels:
+                tooNearBorder = False
+                for j in range(4):
+                    if(approxCurve[j][0] < minDistanceToBorder or approxCurve[j][1] < minDistanceToBorder or approxCurve[j][0] > canny.shape[0] - 1 - minDistanceToBorder or approxCurve[j][1] > canny.shape[1] - 1 - minDistanceToBorder): 
+                        tooNearBorder = True
+
+                if(tooNearBorder): continue
                 x,y,w,h = cv.boundingRect(cnt)
                 area = cv.contourArea(cnt)
-                a = (x,y,w,h, area)
+                
+
+                candidate_corner = []
+                warped =[]
+                for j in range(4):
+                    candidate_corner.append([approxCurve[j][0], approxCurve[j][1]])
+                print("CANDIDATE CORN")
+                
+                candidate_corner = np.array(candidate_corner)
+                candidate_corner = order_points(candidate_corner)
+                warped = four_point_transform(gray, candidate_corner)
+                _, warped = cv.threshold(warped, 125, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+                drawing3 = np.zeros((warped.shape[0], warped.shape[1], 3), dtype=np.uint8)
+                contour_warped, _ = cv.findContours(warped, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)          
+
+                a = (x,y,w,h, area, warped, contour_warped)
                 squares.append(a)
+                for k in range(len(contour_warped)):
+                    #contour_len_warped = cv.arcLength(contour_warped[k], True)
+                    #approx_Curve_warped = cv.approxPolyDP(contour_warped[k], (float)((contour_len_warped) * parameters.polygonalApproxAccuracyRate), True)
+                    #if len(approx_Curve_warped) == 3 and cv.isContourConvex(approx_Curve_warped):
+                    cv.drawContours(drawing3, contour_warped, k, color, 1, cv.LINE_8)
+
+                cv.imshow("draw3", drawing3)
+
+                #k = cv.waitKey(0)
+                #cv.destroyWindow('draw3')
         """
         if len(cnt) == 3:# and cv.countourArea(cnt) > 200:
             xt,yt,wt,ht = cv.boundingRect(cnt)
@@ -153,12 +282,15 @@ while True:
     squares = non_max_supression(squares, 0.5)
    # triangles = non_max_supression(triangles, 0.5)
 
+    markers = match_warped(squares)
+
+    """
     #join squares and triangles and orders them
     shapes = []
     shapes = squares #+ triangles
     shapes = sorted(shapes, key=itemgetter(4))
 
-
+    
     #now, to find the marker (triangle inside square inside square)
     i = len(shapes)-1
     patch = []
@@ -175,9 +307,17 @@ while True:
         h = shapes[i][3]
 
         cv.rectangle(img, (x1,y1), (x1+w,y1+h), (0,255,0),10)
-
+    """
+    for i in range(len(markers)):
+        x1 = markers[i][0]
+        y1 = markers[i][1]
+        w = markers[i][2]
+        h = markers[i][3]
+        cv.rectangle(img, (x1,y1), (x1+w,y1+h), (0,255,0),10)
 
     cv.imshow("window", img)
+
+    
 
 
     #Quebrar se 'q' for premido
