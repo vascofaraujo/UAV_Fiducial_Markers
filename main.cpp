047aba1529122e563000a2df371cb60abd39e991
santinorod <63883410+santinorod@users.noreply.github.com>
2021-04-01 18:08:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,24 +9,51 @@ using namespace cv;
 
 cv::Mat compute_marker(cv::Mat img) 
 {
-	cv::Mat threshImage;
-	std::vector< std::vector< Point > > contours;
+	cv::Mat threshImage; 
+	std::vector<std::vector<cv::Point> > contours;
+	double cnt_len;
 	cv::aruco::DetectorParameters* params = cv::aruco::DetectorParameters::create();
 	adaptiveThreshold(img, threshImage, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 5, 3);
 
 	findContours(threshImage, contours, RETR_LIST, CHAIN_APPROX_NONE);
+	Mat drawing = Mat::zeros(img.size(), CV_8UC3);
+
+	
+	//for (size_t i = 0; i < contours.size(); i++)
+	//{
+		//std::cout << contours[i] << "print \n";
+		//std::cout << "print \n";
+		
+	//}
+
 
 	for (unsigned int i = 0; i < contours.size(); i++) {
 		std::vector< Point > approxCurve;
-		approxPolyDP(contours[i], approxCurve, double(contours[i].size()) * params->polygonalApproxAccuracyRate, true);
-		if (approxCurve.size() != 4 || !isContourConvex(approxCurve)) continue;
+		cnt_len = arcLength(contours[i], true);
+		
+		//if (approxCurve.size() != 4 || !isContourConvex(approxCurve)) continue;
+		//std::cout << "ENCONTREI CONVEX E 4 LADOS \n";
+		//std::cout << "SIZE:" << contours[i].size() << "\n";
+		//if (approxCurve.size() == 4 && isContourConvex(approxCurve)) {
+			
+			//std::cout << "SIZE2:" << cnt_len << "\n";
+			//std::cout << contourArea(contours[i]) << "\n";
+			//std::cout << "COL:" << img.cols << "ROWS" << img.rows << "\n";
+			if (contourArea(contours[i]) > 80 && contourArea(contours[i]) < 0.2 * img.cols * img.rows) {
+				//approxPolyDP(contours[i], approxCurve, double(cnt_len * params->polygonalApproxAccuracyRate), true);
+				//std::cout << "SIZE2:" << params->polygonalApproxAccuracyRate << "\n";
+				approxPolyDP(contours[i], approxCurve, double(contours[i].size()) * 0.03, true);
+				if (approxCurve.size() == 4 && isContourConvex(approxCurve)) {
+					drawContours(drawing, contours, (int)i, (255, 255, 255), 2, LINE_8);
+				}
+				std::cout << approxCurve << "ENCONTREI \n";
 
-		if (contourArea(cv::Mat(contours[i])) > 80 and contourArea(cv::Mat(contours[i])) < 0.2 * img.cols * img.rows) {
-			std::cout << "ENCONTREI \n";
-
+			}
 		}
+		//}
 
-		}
+	cv::imshow("Drawing", drawing);
+	waitKey(0);
 
 
 
